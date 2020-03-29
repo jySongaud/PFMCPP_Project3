74988afe9961fbc347967f46ee15308f4db27a8e
matkatmusic <matkatmusic@gmail.com>
2020-03-29 02:29:12 +0000

    revised part1 instructions

--- main.cpp
+++ main.cpp
@@ -4,21 +4,75 @@ Video:  Chapter 2 Part 5
  User-Defined Types
 
  Create a branch named Part1
+
+ Purpose: The entire purpose of this 5-part project is to get you writing C++ code that compiles and to reinforce the syntax habits that C++ requires.  What you create in this project will be used as the basis of Project 5 in the course.   
  
- 1) write 10 user-defined types, each with a random number of member variables
-    try to make the member variables have names that are related to the user-defined type.
+ 1) write 10 user-defined types in plain-English, 
+    in the comments above your UDT, write out, in plain-english, 3 things it can do, and 5 traits or properties.
+    Note: Your 10th UDT will use 5 of your UDTs for its properties, so start on UDT #10 first and work backwards. This goes along with instruction #6.
+
+    The goal of this step is to get you to think about breaking down an object into smaller and smaller objects, until the smallest object is made of up only primitives. 
+
+    Here is an example:
+    Cell Phone
+
+    A Cell Phone is made up of:
+        Display
+        Memory
+        CPU
+        Radio
+        Applications
+
+    These things make up the 5 traits.
+    These 5 things can be broken down into their own sub-objects and properties. 
+
+    For example: Display
+    A Display has the following traits or properties:
+        pixels
+        amount of power consumed.
+        brightness.
+
+    the Display's brightness can be represented with a Primitive, such as a double. 
+    The amount of power consumed can also be represented with a Primitive.
+    The 'pixels' must be represented with an array of Pixel instances.  Arrays have not been discussed and can't be used in this project.
+    Instead, we can use an Integer primitive to describe the Number of Pixels:
+
+    Display:
+        Number of Pixels
+        Amount of Power consumed
+        Brightness
+
+    Notice that I did not write "has a display" or "Has memory" or "has a cpu"  
+    Writing 'has a ___" checks whether or not your object *has the ability to do something*.
+    Instead, I wrote "Display" or "CPU".  These are specific objects or amounts. 
+    In C++ terms, this means to I want you to avoid bool (has a) as a member variable type.
+    Instead, prefer the other primitive types when you get to step 2)
+************************
+    Commit after you complete this step so i can review before you proceed with the rest of the projects
+    Ping me in Slack to review this step.
+    Wait for me to review before proceeding with step 2.
+************************
+  
+ 2) define your struct for each of your 10 types. 
+    Copy your 3+5 comments into your struct body.
+    comment them out.
  
- 2) give the member variables relevant data types
+ 3) declare your member variables and member functions underneath each plain-english comment in your struct's body.
+    give the member variables relevant data types
  
- 3) add a couple member functions.  
-    make the function parameter list for those member functions use some of your User-Defined Types
+ 4) make the function parameter list for those member functions use some of your User-Defined Types
+    You'll write definitions in part2 for these functions
+    you'll call each of these functions in part3
  
- 4) make 2 of the 10 user-defined types be nested class
+ 5) make 2 of the 10 user-defined types have a nested class.  
+    this nested class also needs member functions.
+    this nested class is not considered one of your 10 UDTs.
  
- 5) One of your UDTs should only use UDTs for its member variable types.   
-     No primitives allowed!
+ 6) One of your 10 UDTs should only use UDTs for its member variable types.   
+    No primitives allowed!
+    it still needs member variables.
  
- 6) After you finish defining each type, click the [run] button.  Clear up any errors or warnings as best you can.
+ 7) After you finish defining each type, click the [run] button.  Clear up any errors or warnings as best you can.
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
@@ -30,206 +84,208 @@ send me a DM to check your pull request
  */
 
 /*
- example:
+ example:  
+
+//1) write out, in plain-english, 5 traits and 3 things it can do.
+Car Wash   
+5 properties:
+    1) has vacuum cleaners
+    2) has eco-friendly cleaning supplies
+    3) stores the amount of water used per week.
+    4) stores amount of profit made per week
+    5) number of cars serviced per day
+
+3 things it can do:
+    1) wash and wax car
+    2) charge customer
+    3) detail the car interior
  */
-struct CarWash            //1) a U.D.T. with a random number of member variables
+struct CarWash //2)        
 {
-    int numSponges = 3;
-    double amountOfSoapUsedPerCar = 2.6; //2) relevant data types
-    unsigned int numCarsProcessed = 0;
+    //2) has vacuum cleaners
+    bool hasVacuumCleaners = true; //3) member variables with relevant data types.
+    //2) has eco-friendly cleaning supplies
+    bool hasEcoFriendlyCleaningSupplies = true; //3) member variables with relevant data types.
+    //2) stores the amount of water used per week.
+    float waterUsedPerWeek = 200.f; //3) member variables with relevant data types.
+    //2) stores amount of profit made per week
+    float profitPerWeek = 495.95f; //3) member variables with relevant data types.
+    //2) number of cars serviced per day
+    int numberOfCarsServiced = 10; //3) member variables with relevant data types.
     
-    struct Car                             //4) nested class
+    //5) make 2 of the 10 user-defined types have a nested class. 
+    struct Car   
     {
+        //2) member variables with relevant data types.  the names are appropriate for the U.D.T.'s purpose.
         bool isAPickupTruck = false;
-        float gasMileage = 26.2f;        //2) relevant data types
+        float gasMileage = 26.2f;        
+
+        //3) a member function whose parameter has a default value.
+        //the parameter name is related to the work the function will perform.
+        void fillTank(double fuelAmountInGallons = 2.0);   
     };
+
+    /* 
+    member functions with a user-defined type as the parameter.
+    The user-defined type parameter happens to be the nested class.
+    */
+
+    //2) wash and wax car
+    void washAndWaxCar( Car car );
+    //2) charge customer
+    float chargeCustomer();
+    //2) detail the car interior
+    void detailInterior( Car car );
     
-    void washAndWaxCar( Car car );         //3) member function with a user-defined type as the parameter.  The user-defined type parameter happens to be the nested class.
-    
-    Car myCar;  //5) a member variable whose type is a UDT.
+               
+    //5) a member variable whose type is a UDT.
+    Car carBeingServiced;  
 };
 
 
 /*
- 1)
+1)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
  */
-struct Playlist
-{
-    int numMusic = 20;
-    bool state = 1;
-    char playMode = 'A';
 
-    struct Music
-    {
-        int orderOfTheMusic = 5;
-        float lengthOfTimeOfTheMusic = 3.55f;
-    
-    };
-
-    void informationOfMusic( Music music );
-
-    Music lastestAddedMusic;
-};
 /*
- 2)
+2)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
  */
-struct Shoppingcart
-{
-    int numItems = 3;
-    float weightItems = 6.99f;
 
-    struct Item
-    {
-        float priceOfItem = 4.67f;
-        bool inStock = 1;
-    };
-    void statusOfItem( Item item );
-
-    Item toiletPaper;
-
-
-};
 /*
- 3)
+3)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
  */
-struct Plan
-{
-    int numTodo = 5;
-    int numDateOfTheYear = 156;
 
-    struct Todo
-    {
-        float timeNeeded = 3.50f;
-        bool completed = 0;
-    };
-    void statusOfTodo( Todo todo);
-
-    Todo quarantine;
-
-};
 /*
- 4)
+4)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
  */
 
-struct OnlineCourse
-{
-    int numChapter = 16;
-    float timeOfCourse = 8.00f;
-
-    struct Assignment
-    {
-        int numRequirement = 5;
-        int dueDate = 135;
-    };
-    void infoAssignment( Assignment assignement);
-
-    Assignment buildABluePrint;
-};
 /*
- 5)
+5)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
  */
-struct Attendence
-{
-    struct SumAttendence
-    {
-        int numStudent = 16;
-        int todayStudent = 14;
-        float percentageAttendence = 0.875f;
-    };
 
-    struct Student
-    {
-        float quiz =9.67f;
-        int numWorkstation = 14;
-
-    };
-    void infoStudent( Student student);
-
-    Student Amanda;
-};
 /*
- 6)
+6)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
  */
-struct AC
-{
-    bool off = 0;
 
-    struct ModeForCool
-    {
-        float currentTemperature = 79.0f;
-        float setTemperature = 74.5f;
-        bool cool = 1;
-    };
-    void autoControlCool( ModeForCool modeForCool);
-
-    ModeForCool exceed745;
-};
 /*
- 7)
+7)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
  */
-struct Shortcuts
-{
-    char brandKeyboard = 'A';
-    char typeKeyboard = 'C';
 
-    struct Function
-    {
-        int howManyKeyToTap = 3;
-        bool tapAllAtOnce = 1;
-    };
-    void searchShortcutForFunction( Function function);
-    Function shuttle;
-};
 /*
- 8)
+8)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
  */
-struct BusService
-{
-    int numRoutes = 5;
 
-
-    struct Stop
-    {
-        int numRoutesHaveThisStop = 2;
-        char TypeRoute1 = 'A';
-        char TypeRoute2 = 'F';
-    };
-    void theClosestStop(Stop stop);
-    Stop BoulderSt;
-};
 /*
- 9)
+9)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
  */
-struct Recipe
-{
-    int numDishesDinner = 4;
 
-    struct Dish
-    {
-        int numVegetables = 3;
-        int numMeat = 1;
-        int numFruits = 0;
-    };
-
-    void IngredDish( Dish dish);
-    Dish SichuanFish;
-};
 /*
- 10)
+10)
+5 properties:
+ These 5 properties should be UDTs that you defined above.
+ this goes along with the instruction:
+    One of your 10 UDTs should only use UDTs for its member variable types.
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
  */
- struct Printer
- {
-     float volumeOfInk = 0.44f;
-     int numPaperCanBePrinted = 380;
 
-     struct Task
-     {
-         int numFile = 2;
-         int numPages = 4;
-     };
-     void statusQuene( Task task);
-     Task brotherHL2390;
- };
 #include <iostream>
 int main()
 {
